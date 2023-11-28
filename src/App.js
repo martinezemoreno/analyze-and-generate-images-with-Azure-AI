51348b771eb87f59ee6d4fda5e6b50b015681bfb
martinxedw <20196700+martinxedw@users.noreply.github.com>
2023-11-28 16:49:34 +0000

    Adding a simple graphical user interface

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Computer vision
+        </p>
+        <p>Insert URL or type prompt:</p>
+        <input type="text" size={70} placeholder="Enter the URL of the image to be analyzed or the prompt of the image to generate"></input><br></br>
+        <button>Analyze</button>
+        <button>Generate</button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
